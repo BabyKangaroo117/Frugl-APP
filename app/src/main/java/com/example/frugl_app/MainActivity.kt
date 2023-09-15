a3d44015baf1452612d610a2762cff459cff6d2a
Syed Mansib Ahmed <mansib.syedahmed@gmail.com>
2023-09-15 13:57:16 -0400

    Created start up and login page

--- app/src/main/java/com/example/frugl_app/MainActivity.kt
+++ app/src/main/java/com/example/frugl_app/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.frugl_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // go to the login page
+        login()
+
+        // go to the sign up page
+        signup()
     }
+
+    private fun login(){
+        val button: Button = findViewById(R.id.login)
+        button.setOnClickListener {
+            val intent: Intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun signup(){
+        val button: Button = findViewById(R.id.signup)
+        button.setOnClickListener {
+            val intent: Intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
