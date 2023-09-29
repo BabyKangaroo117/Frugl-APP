25b256b43395e68e8d211487e78246a902869b0d
Tyler Thompson <tylert123@yahoo.com>
2023-09-29 13:00:38 -0400

    Refactored connection class to be a function.

---
+++ app/src/main/java/com/example/frugl_app/ConnectDB.kt
@@ -0,0 +1,35 @@
+package com.example.frugl_app
+
+import android.os.StrictMode
+import android.util.Log
+import java.lang.Error
+import java.sql.Connection
+import java.lang.Exception
+import java.sql.DriverManager
+import java.sql.SQLException
+
+/**
+ * Connects to a database. Ensure all parameters are correctly entered.
+ */
+fun connectDB(ip: String, databaseName: String, userName: String, password: String): Connection? {
+    val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+    StrictMode.setThreadPolicy(policy)
+    var conn : Connection? = null
+    var connString : String? = null
+    try {
+        Class.forName("net.sourceforge.jtds.jtds.Driver")
+        connString = "jdbc:jtds:sqlserver://$ip;databaseName=$databaseName;user=$userName;password=$password;"
+        conn = DriverManager.getConnection(connString)
+    }
+    catch (ex1 : SQLException) {
+        ex1.message?.let { Log.e("Error : ", it) }
+    }
+    catch (ex2 : ClassNotFoundException) {
+        ex2.message?.let { Log.e("Error : ", it) }
+    }
+    catch (ex3 : Exception) {
+        ex3.message?.let { Log.e("Error : ", it) }
+    }
+
+    return conn
+}
