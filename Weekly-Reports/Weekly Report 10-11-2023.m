82cb55f653f60f0aec7b659138fa2e85f10a9bda
Tyler Thompson <tylert123@yahoo.com>
2023-10-05 12:53:18 -0400

    Create Weekly Report 10-11-2023.m
    Created Weekly Report

---
+++ Weekly-Reports/Weekly Report 10-11-2023.m
@@ -0,0 +1,55 @@
+# Date 10/4/2023
+# Team Report
+
+#### Previous Goals
+- Provide everyone with a way to connect to database through application. This would be without the temporary login information. Must be secure.
+- Decide on how information will be stored. This will determine how much this appliaction will depend on the database. If not information is stored within the application, then the database will only store user information and orders.
+#### Key Points
+- What was done
+- Start to learn how to use projects in GitHub to enhance scrum meetings.
+- What worked
+- What was learned
+- Have trouble with
+- Stuck on
+#### Next Week Goals
+
+# Individual Reports
+
+### Tyler Thompson
+##### Previous Goals
+- Fully learn how to make a scrum work flow in Github.
+- Try and lock in how the database will be used within the application. This will depend on if the information is retrieved when a user processes an order or if a script will run in the morning to update the database with generic items.
+- Will work with Joe to fully flesh out what the application does with the web scrapper data.
+##### Key Points
+
+##### Next Week Goals
+
+
+### Joseph Porrino
+#### Previous Goals
+
+##### Key Points
+
+##### Next Week Goals
+
+### Aaron Feinberg
+#### Previous Goals
+
+##### Key Points
+
+##### Next Week Goals
+
+### Taazkir Nasir
+##### Previous Goals
+
+##### Key Points
+
+##### Next Week Goals
+
+
+### Syed Ahmed
+##### Previous Goals
+
+##### Key Points
+
+##### Next Week Goals
