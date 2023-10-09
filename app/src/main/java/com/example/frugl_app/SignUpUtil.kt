67022e19e332ddfbb3fccf3628fe27e387d5b2f8
aaron <afeinber35@gmail.com>
2023-10-09 12:03:38 -0400

    Add test cases and validation for signup page
    - Implemented test cases to ensure username and password requirements are met during signup.
    
    - Introduced SignUpUtil object with validation function for handling potential invalid signups.

--- app/src/main/java/com/example/frugl_app/SignUpUtil.kt
+++ app/src/main/java/com/example/frugl_app/SignUpUtil.kt
@@ -1,23 +1,80 @@
 package com.example.frugl_app
+
 /**
  *SignUp Unit Testing
  */
+
+/**
+ * Singleton Object to perform validation new signups
+ *
+ *
+ * Sign Up will fail if User:
+ *
+ * -userName:
+ *
+ *      -enters an existing username
+ *
+ *      -leaves userName field empty
+ *
+ * password:
+ *
+ *      -leaves password empty
+ *
+ *      -does not match confirmPassword with password
+ *
+ * @TODO does not meet password strength requirements
+ * @return boolean
+ */
 object SignUpUtil {
+    //password validation regex
     /**
-     * Sign Up will fail if User:
-     * -userName:
-     * -enters an existing username
-     * -leaves userName field empty
-     * password:
-     * -leaves password empty
-     * -does not match confirmPassword with password
-     * -does not meet password strength requirements @TODO
+     * password regex:
+     *
+     *      - atleast 1 lowercase letter
+     *
+     *      - atleast 1 uppercase letter
+     *
+     *      - atleast 1 digit
+     *
+     *      - atleast 1 special character from [!.,?$%^&*]
+     *
+     *      - password must be 8 characters or longer
      */
+    private const val passwordRequirementsPattern = "^(?=.*[a-z])(?=.*\\d)(?=.*[!.,?\$%^&*]).{8,}\$"
+
     fun validatingSignUpInput(
         userName: String,
         password: String,
         confirmPassword: String
     ): Boolean {
+        //USERNAME VALIDATION
+
+        //case for empty user name being entered
+        if (userName.isEmpty()){
+            return false
+        }
+
+        //@TODO case for existing username being entered
+        //...
+
+        //PASSWORD VALIDATION
+
+        //case for empty password being entered
+        if(password.isEmpty()){
+            return false
+        }
+
+        //case for confirmPassword not matching password
+        if(confirmPassword != password){
+            return false
+        }
+
+        //case for password not meeting strength requirements
+        if (!password.matches((passwordRequirementsPattern.toRegex()))){
+            return false
+        }
+
+        // passed all cases...valid return
         return true
     }
 }
--- app/src/test/java/com/example/frugl_app/SignUpUtilTest.kt
+++ app/src/test/java/com/example/frugl_app/SignUpUtilTest.kt
@@ -1,6 +1,5 @@
 package com.example.frugl_app
 
-
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
@@ -8,12 +7,43 @@ class SignUpUtilTest{
 
     //creating a generic valid password for testing cases that reqiure it
     private val validTestPassword: String = "Passoword123"
+
+    //
+    private val newUsers = listOf("mark1@gmail.com")
+
     //generating existing users to test failure of registering with existing username
-    private val existingUsers = listOf("Peter", "Carl")
+    private val existingUsers = listOf("Peter@gmail.com","Carl@hotmail.com")
+
+    //VALID TEST CASE
+    /**Testing for a successful SignUp submission*/
+    @Test
+    fun `valid signUp returns true`(){
+        val result = SignUpUtil.validatingSignUpInput(
+            userName = newUsers[0],
+            password = validTestPassword,
+            confirmPassword = validTestPassword
+        )
+
+        assertThat(result).isTrue()
+    }
+
+    /**Testing empty userName*/
     @Test
     fun `empty userName returns false`(){
+
         val result = SignUpUtil.validatingSignUpInput(
             userName = "",
+            password = validTestPassword,
+            confirmPassword = validTestPassword
+        )
+        assertThat(result).isFalse()
+    }
+    /**Testing user inputing an existing userName*/
+    @Test
+    fun `existing userName returns false`(){
+
+        val result = SignUpUtil.validatingSignUpInput(
+            userName = existingUsers[0],
             password = validTestPassword,
             confirmPassword = validTestPassword
         )
@@ -21,4 +51,41 @@ class SignUpUtilTest{
         assertThat(result).isFalse()
     }
 
+    // testing password cases
+
+    /**Testing empty password entry*/
+    @Test
+    fun `empty password returns false`(){
+        val result = SignUpUtil.validatingSignUpInput(
+            userName = newUsers[0],
+            password = "",
+            confirmPassword = ""   //note that since we are testing a single item - we still match comfirm password to password
+        )
+
+        assertThat(result).isFalse()
+    }
+
+    /**Testing for non-matching confirmation and password*/
+    @Test
+    fun `confirmPassword doesn't match password returns false`(){
+        val result = SignUpUtil.validatingSignUpInput(
+            userName = newUsers[0],
+            password = validTestPassword,
+            confirmPassword = ""
+        )
+
+        assertThat(result).isFalse()
+    }
+
+    /**testing password not meeting strength requirements*/
+    @Test
+    fun `password doesn't meet strength requirements returns false`(){
+        val result = SignUpUtil.validatingSignUpInput(
+            userName = newUsers[0],
+            password = "weak",
+            confirmPassword = "weak"
+        )
+
+        assertThat(result).isFalse()
+    }
 }
