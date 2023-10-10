563c205c4b65b68276a46c25f7f1de61605e45e3
Tyler Thompson <tylert123@yahoo.com>
2023-10-10 17:44:36 -0400

    Update Weekly Report 10-11-2023.m
    Updated Weekly Report

--- Weekly-Reports/Weekly Report 10-11-2023.m
+++ Weekly-Reports/Weekly Report 10-11-2023.m
@@ -8,13 +8,19 @@
 - What was done
   - Talked about how data is stored. Decided to go on unit price of items to get the best overall deal.
   - Redefined the way the webscraper returns information.
+  - Set up and redefined the GitHub projects to make it like scrum sprints.
+  - Set up teams which are appointed to certain aspects of the application.
 - What worked
   - Setting up github actions.
+  - Adding JTBC driver.
 - What was learned
-  - How we want to item prices. It makes it easier to compare the best deals for each store.
+  - How we want to store item prices which is by unit price. It makes it easier to compare the best deals for each store.
 - Have trouble with
+  - Connecting the database with JDBC Driver. Mostly problems with how the connection is established.
 - Stuck on
+  - Allowing access to the database through a pubilc network. Azure has a lot of security features.
 #### Next Week Goals
+- Keep trying to connect database and input sample data.
 
 # Individual Reports
 
@@ -24,9 +30,14 @@
 - Try and lock in how the database will be used within the application. This will depend on if the information is retrieved when a user processes an order or if a script will run in the morning to update the database with generic items.
 - Will work with Joe to fully flesh out what the application does with the web scrapper data.
 ##### Key Points
-
+- Refactored the JTDS driver and replaced it with JDBC driver. This is more updated to work with the Azure database. JDBC driver is the interface used to interact with the database.
+- Created most of the tables for the database. Most of the way the information will be stored is now known.
+- Set up a database team section in the GitHub projects section. Picked date estimates to try and have certain parts of the application completed by.
+- Started to look into JDocs documentation pages that can be created.
 ##### Next Week Goals
-
+- Fully learn JDoc documentation pages.
+- Finish debugging the database connection with JTDS driver.
+- Start adding test data to the database if everything goes smooth.
 
 ### Joseph Porrino
 #### Previous Goals
