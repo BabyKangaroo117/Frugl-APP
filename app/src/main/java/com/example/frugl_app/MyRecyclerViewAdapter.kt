004d328f3e65bee80c600bc4c7da955e714f2fd2
Taazkir <107087417+Taazkir@users.noreply.github.com>
2023-10-25 00:51:55 -0400

    Added MyRecyclerViewAdapter.kt. Implemented functionality to SearchItem.kt. Updated search result view layout to display product name and description separately, and display store logo Added store logo to drawables

---
+++ app/src/main/java/com/example/frugl_app/MyRecyclerViewAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.frugl_app
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyRecyclerViewAdapter(
+    private val context: Context,
+    private val items: List<String>
+) : RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val productDescription: TextView = itemView.findViewById(R.id.productDescription)
+        val fixedPrice: TextView = itemView.findViewById(R.id.fixedPrice)
+        val unitPrice: TextView = itemView.findViewById(R.id.unitPrice)
+        // Add more views if needed
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.activity_search_item_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        // Set data from your items list to the views in your layout
+        holder.productDescription.text = item
+        // Set other data to your other views as needed
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
--- app/src/main/java/com/example/frugl_app/SearchItem.kt
+++ app/src/main/java/com/example/frugl_app/SearchItem.kt
@@ -2,10 +2,56 @@ package com.example.frugl_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class SearchItem : AppCompatActivity() {
+
+    private lateinit var searchView: SearchView
+    private val predefinedItems = arrayOf(
+        "Apple", "Banana", "Cherry", "Date", "Fig", "Grape",
+        "Kiwi", "Lemon", "Mango", "Orange", "Papaya", "Peach", "Pineapple", "Plum", "Raspberry",
+        "Strawberry", "Watermelon"
+    )
+    private val suggestionsList = mutableListOf<String>()
+    private lateinit var adapter: MyRecyclerViewAdapter
+    private lateinit var recyclerView: RecyclerView // Add a reference to the RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_item)
+
+        searchView = findViewById(R.id.searchItemBar)
+        recyclerView = findViewById(R.id.searchRV)
+
+        // Initialize the RecyclerView and set its layout manager
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MyRecyclerViewAdapter(this, suggestionsList)
+        recyclerView.adapter = adapter
+
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    suggestionsList.clear()
+                    for (item in predefinedItems) {
+                        if (item.startsWith(newText, ignoreCase = true)) {
+                            suggestionsList.add(item)
+                        }
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+                return false
+            }
+        })
+
+
     }
 }
+
