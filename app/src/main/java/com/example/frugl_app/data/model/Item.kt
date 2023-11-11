e7b6bc1c5df2f061b4cc55185ad5a39856010241
afeins19 <56986596+afeins19@users.noreply.github.com>
2023-11-10 23:21:10 -0500

    Item Activity now maps to database response

--- app/src/main/java/com/example/frugl_app/data/model/Item.kt
+++ app/src/main/java/com/example/frugl_app/data/model/Item.kt
@@ -4,17 +4,36 @@ import android.os.Parcelable
 
 data class Item(
     val itemName: String,
-    var quantity: Int
+    val itemUnits: String,
+    var quantity: Int,
+    val postalCode: Int,
+
+    //unit prices
+    val shopriteItem: Double,
+    val wegmansItem: Double
+
 ): Parcelable {
     constructor(parcel: android.os.Parcel) : this(
-        parcel.readString()!!,
-        parcel.readInt()
+        parcel.readString()!!, //itemName
+        parcel.readString()!!, //itemUnits
+        parcel.readInt(),      //quantity
+        parcel.readInt(),      //postalCode
+
+        //unit prices
+        parcel.readDouble(),
+        parcel.readDouble()
     ) {
     }
 
     override fun writeToParcel(parcel: android.os.Parcel, flags: Int) {
         parcel.writeString(itemName)
+        parcel.writeString(itemUnits)
         parcel.writeInt(quantity)
+        parcel.writeInt(postalCode)
+
+        //unit prices
+        parcel.writeDouble(shopriteItem)
+        parcel.writeDouble(wegmansItem)
     }
 
     override fun describeContents(): Int {
